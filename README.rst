d4c3d79d3fd8d4288c1d3d6122efba48b7e67955
MikeK <mgkittridge@gmail.com>
2019-06-25 14:14:03 +1200

    initial commit

---
+++ README.rst
@@ -0,0 +1,4 @@
+Repository for running Hilltop web service tests for ECan
+=============================================================
+
+This git repository contains the code running Hilltop web service tests for ECan using TravisCI.
---
+++ tests/test_web_service_test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 30 12:05:46 2018
+
+@author: MichaelEK
+"""
+import pytest
+import pandas as pd
+from hilltoppy.web_service import measurement_list, site_list, get_data, wq_sample_parameter_list
+
+### Parameters
+base_url = 'http://testwateruse.ecan.govt.nz'
+
+hts_names = ['AquiferManualQcEcanDaily', 'AquiferRecorderQcEcanDaily', 'AbstractionRawDaily', 'RiverManualQcEcanDaily', 'RiverRecorderQcEcanDaily', 'AtmosRecorderQcEcanDaily', 'RiverRecorderRawEcanDaily', 'AtmosRecorderRawEcanDaily', 'AquiferRecorderRawEcanDaily', 'AquiferManualQcEcanHourly', 'AquiferRecorderQcEcanHourly', 'RiverManualQcEcanHourly', 'RiverRecorderQcEcanHourly', 'AtmosRecorderQcEcanHourly', 'RiverRecorderRawEcanHourly', 'AtmosRecorderRawEcanHourly', 'AquiferRecorderRawEcanHourly']
+
+### Tests
+
+@pytest.mark.parametrize('hts', [h + '.hts' for h in hts_names])
+def test_site_mtypes(hts):
+    sites = site_list(base_url, hts)
+    site1 = sites.iloc[2].SiteName
+    mtype_df1 = measurement_list(base_url, hts, site1).reset_index().iloc[0]
+    tsdata1 = get_data(base_url, hts, site1, mtype_df1.Measurement, from_date=str(mtype_df1.From), to_date=str(mtype_df1.From))
+    assert len(tsdata1) == 1
+
